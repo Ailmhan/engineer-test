b3aef7be92d67546d5e4c3afdf257b1f48df82e2
Alimhan <56984209+Ailmhan@users.noreply.github.com>
2025-12-29 13:57:11 +0600

    test task results

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,18 @@
 import { createDb } from "./db";
 
-const citySource = [
-  { uuid: "3ba648aa-4498-43da-b29f-b83f37a25429", name: "Алматы" },
-  { uuid: "32d82d73-3eac-4e5a-9921-fcd2e1447c76", name: "Астана" },
-];
+type City = { uuid: string; name: string };
+type Division = { uuid: string; name: string; cityUuid: string };
+type Position = { uuid: string; name: string };
+type Employee = {
+  uuid: string;
+  firstName: string;
+  lastName: string;
+  divisionUuid: string;
+  cityUuid: string;
+  positionUuid: string;
+};
 
-const divisionSource = [
-  {
-    uuid: "97cf9556-2882-4c4a-b7b5-37cf53347447",
-    name: "Департамент информационных технологий",
-    cityUuid: "3ba648aa-4498-43da-b29f-b83f37a25429",
-  },
-  {
-    uuid: "3e80754a-3681-4e5c-8d6d-b84d09a7a3c4",
-    name: "Дирекция",
-    cityUuid: "3ba648aa-4498-43da-b29f-b83f37a25429",
-  },
-];
-
-const positionSource = [
-  {
-    uuid: "3e80754a-3681-4e5c-8d6d-b84d09a7a3c4",
-    name: "Руководитель службы поддержки",
-  },
-  { uuid: "cc811dfb-7f73-4c18-969f-c8408fd92263", name: "Разработчик" },
-];
-
-const employeeSource = [
-  {
-    uuid: "65f5c1d4-fb87-4da2-b0bd-a22343605396",
-    firstName: "Name 1",
-    lastName: "Name 2",
-    divisionUuid: "3e80754a-3681-4e5c-8d6d-b84d09a7a3c4",
-    cityUuid: "3ba648aa-4498-43da-b29f-b83f37a25429",
-    positionUuid: "cc811dfb-7f73-4c18-969f-c8408fd92263",
-  },
-  {
-    uuid: "59e23b74-8645-46d6-9751-5fe594dd89e6",
-    firstName: "Name 1",
-    lastName: "Name 2",
-    divisionUuid: "3e80754a-3681-4e5c-8d6d-b84d09a7a3c4",
-    cityUuid: "3ba648aa-4498-43da-b29f-b83f37a25429",
-    positionUuid: "cc811dfb-7f73-4c18-969f-c8408fd92263",
-  },
-];
-
+type DbType = "employee" | "city" | "position" | "division";
 
 export interface IHRApp {
   employeeWithCityList: () => Promise<{ firstName: string; city: string }[]>;
@@ -59,19 +27,93 @@ export interface IHRApp {
   }) => Promise<void>;
 }
 
+class Repo {
+  constructor(private readonly db: ReturnType<typeof createDb>) {}
+
+  async all<T>(type: DbType): Promise<T[]> {
+    const res = await this.db.query({ type, where: {} as any });
+    return res.items.map((x) => x.data as T);
+  }
+}
+
+class RefCache {
+  private cache = new Map<DbType, Map<string, string>>();
+  private inFlight = new Map<DbType, Promise<Map<string, string>>>();
+
+  constructor(private readonly repo: Repo) {}
+
+  cityMap(): Promise<Map<string, string>> {
+    return this.getMap<City>("city", (x) => x.uuid, (x) => x.name);
+  }
+  positionMap(): Promise<Map<string, string>> {
+    return this.getMap<Position>("position", (x) => x.uuid, (x) => x.name);
+  }
+  divisionMap(): Promise<Map<string, string>> {
+    return this.getMap<Division>("division", (x) => x.uuid, (x) => x.name);
+  }
+
+  private getMap<T>(
+    type: DbType,
+    key: (x: T) => string,
+    value: (x: T) => string
+  ): Promise<Map<string, string>> {
+    const ready = this.cache.get(type);
+    if (ready) return Promise.resolve(ready);
+
+    const inflight = this.inFlight.get(type);
+    if (inflight) return inflight;
+
+    const p = (async () => {
+      const items = await this.repo.all<T>(type);
+      const map = new Map<string, string>();
+      for (const it of items) map.set(key(it), value(it));
+      this.cache.set(type, map);
+      this.inFlight.delete(type);
+      return map;
+    })().catch((e) => {
+      this.inFlight.delete(type);
+      throw e;
+    });
+
+    this.inFlight.set(type, p);
+    return p;
+  }
+}
+
 export const createHRApp = (): IHRApp => {
   const db = createDb();
-
+  const repo = new Repo(db);
+  const refs = new RefCache(repo);
 
   return {
     employeeWithCityList: async () => {
-      return [];
+      const [employees, cities] = await Promise.all([
+        repo.all<Employee>("employee"),
+        refs.cityMap(),
+      ]);
+
+      return employees.map((e) => ({
+        firstName: e.firstName,
+        city: cities.get(e.cityUuid) ?? "",
+      }));
     },
+
     employeeWithPositionList: async () => {
-      return [];
+      const [employees, pos, div] = await Promise.all([
+        repo.all<Employee>("employee"),
+        refs.positionMap(),
+        refs.divisionMap(),
+      ]);
+
+      return employees.map((e) => ({
+        firstName: e.firstName,
+        position: pos.get(e.positionUuid) ?? "",
+        division: div.get(e.divisionUuid) ?? "",
+      }));
     },
-    update: async () => {
-      // этот метод имплементировать не нужно
+
+    update: async (_args) => {
+      // по заданию не нужно
     },
   };
 };
